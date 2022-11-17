9a83ab8264b05103ac8dd659fa5b5adc2d715fd8
sanzous <canastraleonard123@gmail.com>
2022-11-17 14:22:21 -0600

    Styling Fixes

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -5,12 +5,12 @@ const Category = () => {
     console.log(categories)
     return (
         <div className='max-w-[1640px] m-auto px-4 py-12'>
-            <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
+            <h1 className='text-orange-600 font-bold text-4xl text-center'>Categories</h1>
             {/* Categories */}
             <div className='grid grid-cols-2 md:grid-cols-4 gap-6 py-6'>
                 {categories.map((item, index) => (
-                    <div key={item.id} className="bg-gray-100 rounded-lg p-4 flex justify-center items-center">
-                        <h2 className='font-bold sm:text-xl '>{item.name}</h2>
+                    <div key={item.id} className="bg-gray-100 rounded-lg p-4 flex flex-row justify-around  items-center">
+                        <h2 className='font-bold sm:text-xl m-5 max-w-[100px]'>{item.name}</h2>
                         <img src={item.image} alt={item.name}
                             className='w-20' />
                     </div>
